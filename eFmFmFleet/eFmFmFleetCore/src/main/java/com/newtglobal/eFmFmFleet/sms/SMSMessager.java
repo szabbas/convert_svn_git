5e2c3cfd15566776a6763f7d3a5915f39949c62f
krajeev <krajeev@ccd4e1f4-7d2c-0d46-bff6-97578c7d604b>
2016-01-22 10:35:26 +0000

    deleting repo because some directory renames have been done

--- eFmFmFleet/eFmFmFleetCore/src/main/java/com/newtglobal/eFmFmFleet/sms/SMSMessager.java
+++
@@ -1,5 +0,0 @@
-package com.newtglobal.eFmFmFleet.sms;
-
-public interface SMSMessager {
-	public boolean sendMessage(final String smsNumber, final String text,final String requestType) throws Exception;
-}
--- eFmFmFleet/eFmFmRouter/src/main/java/com/newtglobal/eFmFmFleetRouter/routing/CostInterface.java
+++
@@ -1,7 +0,0 @@
-package com.newtglobal.eFmFmFleetRouter.routing;
-
-import com.newtglobal.eFmFmFleetRouter.clustering.Geocode;
-
-public interface CostInterface {
-	public Cost getCost(Geocode G1, Geocode G2);
-}
