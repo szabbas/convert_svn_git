25b378b913fc1509c6636b71abba6fe7ec05ae01
krajeev <krajeev@ccd4e1f4-7d2c-0d46-bff6-97578c7d604b>
2016-02-10 16:32:19 +0000

    target not required

--- eFmFmFleet/eFmFmFleetWeb/src/main/webapp/scripts/directives/href.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    angular.module('efmfmApp').directive('href', function() {
-    return {
-     compile: function(element) {
-       element.attr('target', '_blank');
-     }
-    };
-   });
-}());
