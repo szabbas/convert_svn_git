2acac09692b2a67ef9f0cb0a390f5c17d6ae2c8c
krajeev <krajeev@ccd4e1f4-7d2c-0d46-bff6-97578c7d604b>
2016-02-13 14:48:18 +0000

    Created Common service for IOS and Android Notifications.

--- eFmFmFleet/eFmFmFleetCore/src/main/java/com/newtglobal/eFmFmFleet/eFmFmFleet/PushNotificationService.java
+++ eFmFmFleet/eFmFmFleetCore/src/main/java/com/newtglobal/eFmFmFleet/eFmFmFleet/PushNotificationService.java
@@ -8,6 +8,8 @@ import org.apache.commons.logging.LogFactory;
 import com.google.android.gcm.server.Message;
 import com.google.android.gcm.server.Result;
 import com.google.android.gcm.server.Sender;
+import com.notnoop.apns.APNS;
+import com.notnoop.apns.ApnsService;
 
 public class PushNotificationService {
 
@@ -20,15 +22,29 @@ public class PushNotificationService {
 	// GCM RedgId of Android device to send push notification
 	try {
 			Sender sender = new Sender(GOOGLE_SERVER_KEY);
-			log.info("Push Send to "+new Date());
 			Message message = new Message.Builder().timeToLive(30)
 					.delayWhileIdle(true).addData(MESSAGE_KEY, userMessage).build();
 			result = sender.send(message, deviceToken, 1);
+			log.info("Android Push Send to "+new Date()+" push status"+result);
 		} catch (Exception ioe) {
-		log.info("Push Exception Block"+ioe);
+		log.info("Android Push Exception Block"+ioe);
 		}
-	
-		
-	
 }
+	
+	
+	public  void iosPushNotification(String token,String msg){
+		try{
+		ApnsService service = APNS
+				.newService()
+				.withCert("/home/newtglobal/Dev/eFmFmPushNotification.p12",
+						"newt@123").withSandboxDestination().build();
+		String payload = APNS.newPayload().alertBody(msg).build();
+		service.push(token, payload);
+		log.info("IOS Push Send to "+new Date()+" push status"+service.toString());
+
+		}catch(Exception e){
+			log.info("IOS Push Exception Block"+e);	
+		}
+	}
+	
 }
